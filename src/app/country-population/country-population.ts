f57aa8f6ca83d42966a214ee943b827d9a0c8678
Erin <erinjmaldonado@gmail.com>
2025-10-24 17:02:37 -0700

    feat: Update country population component with data fetching and display

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PopulationData } from './population-data';
 import { environment } from '../../environments/environment';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
 
-export class CountryPopulation {
-  Country!: PopulationData;
-  constructor(http: HttpClient) {
-    http.get<PopulationData>(environment.apiUrl + 'api/countries/population/26').subscribe(result => {
-      this.Country = result;
+export class CountryPopulation implements OnInit {
+  CountryPopulation!: PopulationData;
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+   
+  }
+
+  ngOnInit(): void {
+    let idparam = this.activatedRoute.snapshot.paramMap.get('id');
+     this.http.get<PopulationData>(`${environment.apiUrl}api/countries/population/${idparam}`).subscribe(result => {
+      this.CountryPopulation = result;
     });
   }
 }
